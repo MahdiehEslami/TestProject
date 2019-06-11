15ec6b9d664155d32f7e795f5391e6087c2e9dc2
S.Mahdi Hoseini <mmhf6380@hotmail.com>
2019-06-11 16:35:48 +0430

    Add Tag Interface Repository

---
+++ TestProject/Phonbook.Core.Contracts/Tags/ITagRepository.cs
@@ -0,0 +1,15 @@
+﻿using PhoneBook.Core.Entites.Tags;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phonbook.Core.Contracts.Tags
+{
+    public interface ITagRepository
+    {
+        Tag Get(int id);
+        List<Tag> GetAll();
+        void Delete(int id);
+        Tag Add(Tag tag);
+    }
+}
