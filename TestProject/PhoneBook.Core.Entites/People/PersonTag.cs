f9844e8b4b74cbfe8210078fb741c6cf0ca6498f
ASUS <ASUS@DESKTOP-40EEQ86>
2019-06-11 03:08:39 -0700

    Add people,tag Entities

---
+++ TestProject/PhoneBook.Core.Entites/People/PersonTag.cs
@@ -0,0 +1,16 @@
+﻿using PhoneBook.Core.Entites.Tags;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneBook.Core.Entites.People
+{
+    public class PersonTag
+    {
+        public int PersonTagId { get; set; }
+        public int TagId { get; set; }
+        public int PersonId { get; set; }
+        public Person person { get; set; }
+        public Tag tag { get; set; }
+    }
+}
---
+++ TestProject/PhoneBook.Core.Entites/Tags/Tag.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneBook.Core.Entites.Tags
+{
+    public class Tag
+    {
+        public int TagId { get; set; }
+        public string Title { get; set; }
+    }
+}
