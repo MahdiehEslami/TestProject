8b375e019718a6f7b3acb116ed483de9238e9bda
ASUS <ASUS@DESKTOP-40EEQ86>
2019-06-25 03:37:49 -0700

    Add PeopleService

---
+++ TestProject/Phonbook.Core.Contracts/People/IPeopleService.cs
@@ -0,0 +1,14 @@
+﻿using PhoneBook.Core.Entites.People;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phonbook.Core.Contracts.People
+{
+   public interface IPeopleService
+    {
+        Person GetPersonWithPhoneList(int Id);
+        void DeletePerson(int Id);
+       // Person AddPersonWithUniqEmail(Person person, String Email);
+    }
+}
---
+++ TestProject/Phonebook.Services.ApplicationServices/PeopleService.cs
@@ -0,0 +1,29 @@
+﻿using Phonbook.Core.Contracts.People;
+using PhoneBook.Core.Entites.People;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phonebook.Services.ApplicationServices
+{
+    public class PeopleService : IPeopleService
+    {
+        private readonly IPersonRepository personRepo;
+
+        public PeopleService(IPersonRepository personRepository)
+        {
+            personRepo = personRepository;
+        }
+        public void DeletePerson(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Person GetPersonWithPhoneList(int Id)
+        {
+            throw new NotImplementedException();
+            //return personRepo.Get(Id).phones.Exists()
+            //.Include(c => c.phones).FirstOrDefault();
+        }
+    }
+}
