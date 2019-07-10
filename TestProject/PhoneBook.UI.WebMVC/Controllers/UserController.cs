8e9b44aae6aebb5047c5fa60f577c249a0aca6e9
ASUS <ASUS@DESKTOP-40EEQ86>
2019-07-10 03:31:48 -0700

    login

--- TestProject/PhoneBook.UI.WebMVC/Controllers/UserController.cs
+++ TestProject/PhoneBook.UI.WebMVC/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using PhoneBook.UI.WebMVC.Models.AAA;
@@ -10,6 +11,7 @@ using PhoneBook.UI.WebMVC.Models.AAA;
 
 namespace PhoneBook.UI.WebMVC.Controllers
 {
+    [Authorize]
     public class UserController : Controller
     {
         private readonly UserManager<AppUser> userManager;
